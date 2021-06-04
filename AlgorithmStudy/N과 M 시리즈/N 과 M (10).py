0b842e776010f195398b47096e508c9e14b05c01
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-06-04 23:31:12 +0900

    6.4

---
+++ AlgorithmStudy/N과 M 시리즈/N 과 M (10).py
@@ -0,0 +1,28 @@
+
+
+n,m = map(int, input().split())
+card = list(map(int, input().split()))
+card.sort()
+
+used = [0] * m
+visited = [0] * n
+
+def solve(stage) :
+    check = 0
+    if stage == m :
+        for i in used :
+            print(card[i], end = " ")
+        print()
+        return
+
+    for i in range(n) :
+        if stage > 0 and i < used[stage-1] :
+            continue
+        if visited[i] == 0 and check != card[i] :
+            visited[i] = 1
+            check = card[i]
+            used[stage] = i
+            solve(stage + 1)
+            visited[i] = 0
+
+solve(0)
