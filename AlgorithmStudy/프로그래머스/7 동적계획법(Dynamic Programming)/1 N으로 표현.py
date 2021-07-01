59a7f4d5db3207a41b1b269798aa50ba56dfbd3b
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-07-01 21:46:08 +0900

    7.1 N으로 표현 다시 풀기

--- AlgorithmStudy/프로그래머스/7 동적계획법(Dynamic Programming)/1 N으로 표현.py
+++ AlgorithmStudy/프로그래머스/7 동적계획법(Dynamic Programming)/1 N으로 표현.py
@@ -1,50 +1,43 @@
 
-# N = 5
-# number = 12
-
 N = 5
-number = 31168
+number = 12
+
+# N = 5
+# number = 31168
 
 def solution(N, number) :
-    answer = 0
+    s = [0, {N}]
 
     if N == number :
         return 1
 
-    dp = [[] for _ in range(9)]
-
-    for i in range(1,9) :
-        strn = str(N)
-
-        newnum = strn * i
-        dp[i].append(int(newnum))
-        dp[2].append(N)
-        # print(dp[i])
-
-        if number in dp[i] :
-            answer = i
-            print(answer)
-            return answer
-
-        if i == 8 :
-            answer = -1
-            print(answer)
-            return answer
-
-        for j in dp[i] :
-            for k in dp[i] :
-                dp[i+1].append(j+k)
-                dp[i+1].append(j*k)
-                dp[i+1].append(abs(j-k))
-                if k == 0 :
-                    continue
-                dp[i+1].append(int(j/k))
-
-        newdp = set(dp[i+1])
-        dp[i+1] = list(newdp)
+    for i in range(2,9) :
+        # N의 자릿수 추가
+        case_set = {int(str(N)*i)}
+        # i의 절반만 돈다.
+        # i = 4
+        # i_half = 1,2
+        for i_half in range(1, i//2+1) :
+            for x in s[i_half] :
+                # i - i_half = 3,2
+                for y in s[i - i_half] :
+                    # 즉 12 랑 34를 비교하게 된다.
+                    case_set.add(x+y)
+                    case_set.add(x-y)
+                    case_set.add(y-x)
+                    case_set.add(x*y)
+                    if x != 0 :
+                        case_set.add(y/x)
+                    if y != 0 :
+                        case_set.add(x/y)
+        if number in case_set :
+            return i
+        s.append(case_set)
+    return -1
 
 
-    print(answer)
-    return answer
 
-solution(N, number)
+
+
+
+print(solution(N, number))
