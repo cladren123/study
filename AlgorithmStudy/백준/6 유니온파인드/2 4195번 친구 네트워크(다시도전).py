80d65e93eb292738f808b3410964db879795403a
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-07-16 17:58:01 +0900

    17472 다리 만들기2

--- AlgorithmStudy/백준/6 유니온파인드/2 4195번 친구 네트워크(다시도전).py
+++ AlgorithmStudy/백준/6 유니온파인드/2 4195번 친구 네트워크(다시도전).py
@@ -1,2 +1,52 @@
 
 
+
+def find(x) :
+    if x == parents[x] :
+        return x
+    else :
+        rootx = find(parents[x])
+        parents[x] = rootx
+        return parents[x]
+
+def union(x,y) :
+    rootx = find(x)
+    rooty = find(y)
+
+    if rootx != rooty :
+        parents[rooty] = rootx
+        number[rootx] += number[rooty]
+
+test_cases = int(input())
+
+for _ in range(test_cases) :
+    parents = dict()
+    number = dict()
+    n = int(input())
+
+
+    for _ in range(n) :
+        f1, f2 = input().split()
+        parents[f1] = f1
+        parents[f2] = f2
+        union(f1, f2)
+
+        count = 0
+        for i in parents :
+            if parents[i] == f2 :
+                count += 1
+
+        print(parents)
+        print(count)
+
+
+
+
+
+
+
+
+
+
+
+
