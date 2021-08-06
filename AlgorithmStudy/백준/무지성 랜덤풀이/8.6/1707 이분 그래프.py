61fbb0690df065e61e7215d24fb71d8247ea318e
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-08-07 01:11:52 +0900

    백준 1707 이분 그래프 BFS

--- AlgorithmStudy/백준/무지성 랜덤풀이/8.6/1707 이분 그래프.py
+++ AlgorithmStudy/백준/무지성 랜덤풀이/8.6/1707 이분 그래프.py
@@ -1,58 +1,66 @@
 """
-깊이 우선 탐색 - DFS
+깊이 우선 탐색 - DFS 인데 bfs로 풀을래
 
 
 """
 import copy
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
-
-visited = [0] * (v + 1)
-
-graph = [[0] * (v+1) for _ in range(v+1)]
-
-for _ in range(e) :
-    start, end = map(int, input().split())
-
-    graph[start][end] = 1
-    graph[end][start] = 1
+testcase = int(input())
 
 
-for i in graph :
-    print(i)
+def bfs(start):
+
+    que = deque()
+    que.append(start)
+    visited[start] = 1
 
 
-def dfs(node) :
-    global v, graph
+    while que:
+        cur = que.popleft()
+        nownum = visited[cur]
 
-    visited[node] = 1
-    print(node, end = " ")
+        for nextv in graph[cur]:
+            if visited[nextv] == 0:
+                if visited[cur] == 1:
+                    visited[nextv] = 2
+                    que.append(nextv)
+                elif visited[cur] == 2:
+                    visited[nextv] = 1
+                    que.append(nextv)
+            else:
+                if visited[nextv] == nownum:
+                    return False
+
+    return True
+
+
+for _ in range(testcase) :
+    v, e = map(int, input().split())
+
+    flag = 0
+
+    graph = [[] for _ in range(v+1)]
+    visited = [0] * (v + 1)
+
+    for _ in range(e) :
+        start, end = map(int, input().split())
+        graph[start].append(end)
+        graph[end].append(start)
 
     for i in range(1, v+1) :
-        if graph[node][i] == 1 and visited[i] == 0 :
-            dfs(i)
+        if visited[i] == 0 :
+            if bfs(i) == False :
+                flag = 1
+                break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if flag == 1 :
+        print("NO")
+    else :
+        print("YES")
 
 
 
