ccd1594da07d3dc86e3044709e7d1e01b3cbc576
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-08-12 10:55:08 +0900

    백준 16564 히오스 프로젝트

---
+++ AlgorithmStudy/백준/무지성 랜덤풀이/8.12/16564 히오스 프로젝트.py
@@ -0,0 +1,52 @@
+import sys
+
+"""
+k를 분배해서 레벨업을 한다.
+
+"""
+
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+level = []
+
+for _ in range(n) :
+    level.append(int(input()))
+
+
+# level 을 이분탐색 해야한다.
+
+start = 1
+end = 1000000000
+
+while start <= end :
+    mid = (start + end) // 2
+
+    res = 0
+
+    for i in level :
+        if i > mid :
+            continue
+        else :
+            res += mid - i
+
+    if k >= res :
+        start = mid + 1
+    else :
+        end = mid - 1
+
+print(end)
+
+
+
+
+
+
+
+
+
+
+
+
