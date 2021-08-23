970ba1e0a2b51b4c748340f0a18b03e3593799bf
cladren <gold3987@naver.com>
2021-08-23 22:36:52 +0900

    defaultdict 정리

--- AlgorithmStudy/백준/무지성 랜덤풀이/8.23/2637 장난감 조립.py
+++ AlgorithmStudy/백준/무지성 랜덤풀이/8.23/2637 장난감 조립.py
@@ -1,8 +1,11 @@
 """
 
-문제 유형
+문제 유형 : 다이나믹프로그래밍, 그래프 이론, 위상 정렬
 
-"""
+
+
+메모리 초과 발생
+
 import sys
 from collections import deque
 
@@ -23,27 +26,67 @@ for _ in range(m) :
     gibonx = min(gibonx, x)
     graph[x].append([y,k])
 
-    que.append([x,y,k])
+    if x == n :
+        que.append([y,k])
 
 gibon = [0] * (gibonx)
 
-for i in graph :
-    print(i)
 
 while que :
-    x,y,k = que.popleft()
+    y,k = que.popleft()
 
-    if x < gibonx :
+    if y < gibonx :
         gibon[y] += k
 
-    elif x >= gibonx :
-        for _ in range()
+    elif y >= gibonx :
+        for _ in range(k) :
+            for i in graph[y] :
+                que.append([i[0], i[1]])
 
 
 
+for i in range(1,gibonx) :
+    print("%d %d" %(i, gibon[i]))
+
+"""
+import sys
+from collections import deque, defaultdict
+
+input = sys.stdin.readline
+
+
+n = int(input())
+m = int(input())
+
+graph = defaultdict(list)
+
+gibonx = 101
+
+que = deque()
+
+for _ in range(m) :
+    x,y,k = map(int, input().split())
+    gibonx = min(gibonx, x)
+    graph[x].append([y,k])
+
+    if x == n :
+        que.append([y,k])
+
+gibon = [0] * (gibonx)
+
+
+while que :
+    y,k = que.popleft()
+
+    if y < gibonx :
+        gibon[y] += k
+
+    elif y >= gibonx :
+        for _ in range(k) :
+            for i in graph[y] :
+                que.append([i[0], i[1]])
 
 
 
-
-print(gibon)
-
+for i in range(1,gibonx) :
+    print("%d %d" %(i, gibon[i]))
