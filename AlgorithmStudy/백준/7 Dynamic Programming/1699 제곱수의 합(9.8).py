8f9a04edadc61b203046458f8ed6ca6368d19b41
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-09-09 09:48:09 +0900

    백준 15988 1,2,3 더하기 3

--- AlgorithmStudy/백준/7 Dynamic Programming/1699 제곱수의 합(9.8).py
+++ AlgorithmStudy/백준/7 Dynamic Programming/1699 제곱수의 합(9.8).py
@@ -20,22 +20,18 @@ DP를 활용해보자
 
 """
 import sys
+from math import sqrt
 
 input = sys.stdin.readline
 
-# 자연수 n 입력
 n = int(input())
 
-# 리스트를 n의 자리까지 만들기 위해 n+1 크기의 리스트 생성
 dp = [i for i in range(n+1)]
-
 
 for i in range(1, n+1) :
     for j in range(1,i) :
-        # 탐색할 제곱수가 i보다 크면 더 이상 탐색이 불필요하기 때문에 중단한다.
         if j*j > i :
             break
-        # 제곱수를 뺐을 때 조건을 만족하면 해당 위치에 +1 한 값으로 교체한다.
         if dp[i] > dp[i-j*j] + 1 :
             dp[i] = dp[i-j*j] + 1
 
