39853cec7fa9e9ef836551bbaa3a800ba97fe0e6
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-09-23 15:01:49 +0900

    백준 1254 팰린드롬

--- AlgorithmStudy/백준/무지성 랜덤풀이/9월/9.23/1254 팰린드롬.py
+++ AlgorithmStudy/백준/무지성 랜덤풀이/9월/9.23/1254 팰린드롬.py
@@ -1,14 +1,28 @@
 
+"""
 
-s = input()
-n = len(s)
+문제유형 :
+문자열
+브루트포스 알고리즘
+
+abab
+
+ababa
+a를 붙혔으니 a는 관찰할 필요가 없음
+
+bab 랑  bab를 뒤집은거랑 비교한다
+
+"""
+import sys
+
+input = sys.stdin.readline
+
+words = input().strip()
+n = len(words)
+
+
 
 for i in range(n) :
-
-    print(s[i:])
-    print(s[i:][::-1])
-
-    if s[i:] == s[i:][::-1] :
-
+    if words[i:] == words[i:][::-1] :
         print(n+i)
         break
