706b99d45451d1df4d34d7b366b617ee08ef28fe
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-09-30 14:30:57 +0900

    java repository

---
+++ javarepository/study/src/baekjoon/helloworld.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+public class helloworld {
+
+//    main 키워드를 사용하면 쉽게 만들 수 있다.
+    public static void main(String[] args) {
+
+        // 출력하기 위해 ctrl + F9 를 눌러 빌드를 한다.
+        // 빌드가 완료되면 out > production > 플젝이름? > class 파일이 생긴 다
+        // ctrl shift 10 사용해 실행한다
+
+        // sout 키워드 사용
+        System.out.println("Hello world");
+        System.out.println("강한친구 대한육군");
+
+    }
+
+    // 만약 모듈의 클래스패스가 안잡혀 있다면 메인 클래스가 생성되어 있는 클래스패스를 지정하면 된다.
+
+}
