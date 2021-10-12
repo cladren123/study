01b5e04dd508425ce4128b70a7fbf4dc92800f99
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-10-12 16:31:12 +0900

    백준 3190 뱀

---
+++ AlgorithmStudy/백준/3 구현/3190 뱀(10.12).py
@@ -0,0 +1,90 @@
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+# n : 보드의 크기
+n = int(input())
+
+# k : 사과의 개수
+k = int(input())
+
+klist = []
+for _ in range(k) :
+    klist.append(list(map(int, input().split())))
+
+# l : 방향 변환 정보
+l = int(input())
+
+llist = deque()
+for _ in range(l) :
+    # 숫자도 문자로 입력되어 버렸다.
+    t, d = input().split()
+    t = int(t)
+    llist.append([t,d])
+
+board = [[0] * (n+1) for _ in range(n+1)]
+
+# 사과배치
+for y,x in klist :
+    board[y][x] = 1
+
+# 시계 방향
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+# 몸 길이
+body = deque()
+body.append([1,1])
+
+# 처음 시작 방향 오른쪽
+d = 0
+
+answer = 0
+
+while True :
+    # 머리 이동
+    hy,hx = body[0][0], body[0][1]
+
+    nhx = hx + dx[d]
+    nhy = hy + dy[d]
+
+    answer += 1
+
+    if 1 <= nhy < (n+1) and 1 <= nhx < (n+1) and [nhy,nhx] not in body:
+        if board[nhy][nhx] == 1 :
+            body.appendleft([nhy,nhx])
+            board[nhy][nhx] = 0
+        else :
+            body.appendleft([nhy,nhx])
+            body.pop()
+    else :
+        break
+
+    # 방향 전환
+    if len(llist) > 0 :
+        if answer == llist[0][0] :
+            # 오른쪽 회전
+            if llist[0][1] == "D" :
+                d += 1
+                if d > 3 :
+                    d = 0
+            # 왼쪽 회전
+            elif llist[0][1] == "L" :
+                d -= 1
+                if d < 0 :
+                    d = 3
+            llist.popleft()
+
+print(answer)
+
+
+
+
+
+
+
+
+
+
