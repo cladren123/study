b1570b50c38cba9e223da610c242c88320e00076
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-10-25 15:28:04 +0900

    백준 9184 신나는 함수 실행:

--- AlgorithmStudy/백준/18 정렬/18870 좌표 압축(10.24).py
+++ AlgorithmStudy/백준/18 정렬/18870 좌표 압축(10.24).py
@@ -7,34 +7,16 @@ input = sys.stdin.readline
 n = int(input())
 nlist = list(map(int, input().split()))
 
+# 랭크를 매긴다음 dict에 ㅓ장
 nrank = list(set(nlist))
 nrank.sort()
-
 nr = len(nrank)
-
 rankdict = dict()
-
 for i in range(nr) :
     rankdict[nrank[i]] = i
 
+# 랭크를 기반으로 압축
 for i in range(n) :
     nlist[i] = rankdict[nlist[i]]
 
 print(*nlist)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
